5912bb23a2f63309a049e8b79af7e5610292f497
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2024-02-28 19:01:18 +0530

    feat: Project setup

---
+++ frontend/esbuild.config.js
@@ -0,0 +1,6 @@
+// esbuild.config.js
+module.exports = {
+  entryPoints: ["src/index.js"],
+  bundle: true,
+  outfile: "dist/bundle.js",
+};
---
+++ frontend/src/pages/Doctors/Doctor.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Doctor = () => {
+  return (
+    <div>
+      <h3>doctor page</h3>
+    </div>
+  )
+}
+
+export default Doctor
